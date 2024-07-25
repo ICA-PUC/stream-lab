c7bd17125841a3497227e0719b274b028d6b9e72
Lucas Mayer <lucasceschini@ica.ele.puc-rio.br>
2024-07-25 08:13:08 -0300

    fix: quality measures to ff.log file

--- viewer/psnr_measure.sh
+++ viewer/psnr_measure.sh
@@ -3,10 +3,10 @@
 echo "Measuring PSNR and MSSIM..."
 ffmpeg -i /viewer/out_stream.mp4 /viewer/out_stream.yuv
 
-ffmpeg -pixel_format yuv420p \
+FFREPORT=file=/viewer/ff.log:level=56 ffmpeg -pixel_format yuv420p \
     -video_size 640x320 \
     -i /viewer/out_stream.yuv \
     -pixel_format yuv420p \
     -video_size 640x320 \
     -i /viewer/ciro-23.yuv \
-    -lavfi "[0][1]ssim;[0][1]psnr" -f null - >> /viewer/ff.log
+    -lavfi "[0][1]ssim;[0][1]psnr" -f null -
